0fc94cd9d5f275656f8983c983cb555b2d4487dc
cakesaryum2 <ryan.yang@my.bdsc.school.nz>
2025-05-08 09:28:20 +1200

    initial commit

---
+++ bank_account_simulator.py
@@ -0,0 +1,8 @@
+# This is a simple bank account simulator that allows users to create an account, deposit money, withdraw money, and check their balance.
+
+#use constants
+#explain why the code is used
+
+from tkinter import *
+from tkinter import messagebox
+import matplotlib
