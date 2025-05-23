5586df8f386d70932f02e91ccc3bdb9e3c81d70b
cakesaryum2 <ryan.yang@my.bdsc.school.nz>
2025-05-23 12:15:55 +1200

    withdraw button and changes to how the gui works

--- bank_account_simulator.py
+++ bank_account_simulator.py
@@ -1,6 +1,5 @@
 # This is a simple bank account simulator that allows users to create an account, deposit money, withdraw money, and check their balance.
 
-#use constants
 #explain why the code is used
 
 from tkinter import *
@@ -14,42 +13,30 @@ amount = 0
 deposit_amount = 0
 
 
-#function for withdrawing money
-def withdraw(balance,amount):
-    while True:
-        try:
-            amount = int(input("Enter amount to withdraw: "))
-            break
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
-        
-    if amount > balance:
-        print("Insufficient funds")
-    else:
-        balance -= amount
-        transaction_histroy.append(f"Withdrew {amount}")
-    return balance
-
-        
 #function for depositing money
 def deposit():
-
+    #deletes inputs for withdraw to prevent overlapping	
+    deposit_button.grid_forget()
+    withdraw_button.grid_forget()
+    history_button.grid_forget()
+    cancel_button = Button(root, text="Cancel", command=lambda:cancel(entered_amounts,amount_label,submit_button,cancel_button))
+    cancel_button.grid(row=4, column=1, padx=5, pady=10)
     #sets up the label and entry box for the deposits
-    deposit_amounts = Entry()
-    deposit_amounts.grid(row=2, column=0, padx=10, pady=10,columnspan=4,)
+    entered_amounts = Entry()
+    entered_amounts.grid(row=2, column=0, padx=10, pady=10,columnspan=4,)
     amount_label = Label(root, text="Enter amount to deposit: ")
     amount_label.grid(row=1, column=0, padx=10, pady=10,columnspan=4)
-    submit_button = Button(root, text="Submit", command=lambda: deposit_submit(deposit_amounts,amount_label,submit_button))
+    submit_button = Button(root, text="Submit", command=lambda: deposit_submit(entered_amounts,amount_label,submit_button,cancel_button))
     submit_button.grid(row=3, column=0, padx=10, pady=10,columnspan=4)
 
 
 
 #function to submit deposit 
-def deposit_submit(deposit_amounts,amount_label,submit_button):
+def deposit_submit(entered_amounts,amount_label,submit_button,cancel_button):
     global balance
     try:
         #checks if the deposit amount is a number
-        deposit_amount = int(deposit_amounts.get())
+        deposit_amount = int(entered_amounts.get())
         if deposit_amount < 0:
             #if the amount is negative it will give an error
             raise ValueError
@@ -57,31 +44,80 @@ def deposit_submit(deposit_amounts,amount_label,submit_button):
         transaction_histroy.append(f"Deposited {deposit_amount}")
         shown_balance.config(text=f"Your balance is: ${balance}")
         #removing the deposit amount entry and label
-        deposit_amounts.destroy()  
+        entered_amounts.destroy()  
         amount_label.destroy()  
         submit_button.destroy() 
         label.config(text="deposit succesful",fg="black")
+        #returning the buttons
+        deposit_button.grid(row=4, column=0, padx=5, pady=10)
+        withdraw_button.grid(row=4, column=1, padx=5, pady=10)
+        history_button.grid(row=4, column=2, padx=5, pady=10)
+        cancel_button.destroy()
     except ValueError:
         label.config(text="Invalid input. Please enter a valid number.")
 
 
+#function for withdrawing money
+def withdraw():
+    #deletes inputs for deposit to prevent overlapping
+    deposit_button.grid_forget()
+    withdraw_button.grid_forget()
+    history_button.grid_forget()
+    #sets up the label and entry box for the withdraw
+    entered_amounts = Entry()
+    entered_amounts.grid(row=2, column=0, padx=10, pady=10,columnspan=4,)
+    amount_label = Label(root, text="Enter amount to withdraw: ")
+    amount_label.grid(row=1, column=0, padx=10, pady=10,columnspan=4)
+    submit_button = Button(root, text="Submit", command=lambda: withdraw_submit(entered_amounts,amount_label,submit_button,cancel_button))
+    submit_button.grid(row=3, column=0, padx=10, pady=10,columnspan=4)
+    cancel_button = Button(root, text="Cancel", command=lambda:cancel(entered_amounts,amount_label,submit_button,cancel_button))
+    cancel_button.grid(row=4, column=1, padx=5, pady=10)
+
+
 #function to submiot withdraw
-def withdraw_submit(withdraw_amount):
+def withdraw_submit(entered_amounts,amount_label,submit_button,cancel_button):
     global balance
-    balance -= int(deposit_amount.get())
-    if balance < 0:
-        label.config(text="Insufficient funds")
-    print(balance)
-    print(f"Deposited {deposit_amount.get()}")
-    transaction_histroy.append(f"Deposited {deposit_amount.get()}")
-    shown_balance.config(text=f"Your balance is: ${balance}")
+    try:
+    #checks if the deposit amount is a number
+        withdraw_amount = int(entered_amounts.get())
+        if withdraw_amount < 0:
+            #if the amount is negative it will give an error
+            raise ValueError
+        elif withdraw_amount > balance:
+            #if the amount is more than the balance it will give an error
+            raise ValueError
+        balance -= int(withdraw_amount)
+        transaction_histroy.append(f"withdrew {withdraw_amount}")
+        shown_balance.config(text=f"Your balance is: ${balance}")
+        #removing the deposit amount entry and label
+        entered_amounts.destroy()  
+        amount_label.destroy()  
+        submit_button.destroy() 
+        label.config(text="withdraw succesful",fg="black")
+        #returning the buttons
+        deposit_button.grid(row=4, column=0, padx=5, pady=10)
+        withdraw_button.grid(row=4, column=1, padx=5, pady=10)
+        history_button.grid(row=4, column=2, padx=5, pady=10)
+        cancel_button.destroy()
+    except ValueError:
+        if withdraw_amount > balance:
+            label.config(text="insufficient funds",fg="red")
+        else:
+            label.config(text="Invalid input. Please enter a valid number.")
 
+def cancel(entered_amounts,amount_label,submit_button,cancel_button):
+    #removing the deposit amount entry and label
+    entered_amounts.grid_forget()  
+    amount_label.grid_forget()  
+    submit_button.grid_forget() 
+    label.config(text="cancelled",fg="black")
+    #recreating the buttons
+    deposit_button.grid(row=4, column=0, padx=5, pady=10)
+    withdraw_button.grid(row=4, column=1, padx=5, pady=10)
+    history_button.grid(row=4, column=2, padx=5, pady=10)
+    #removing itself
+    cancel_button.destroy()
 
-
-    
-#function to show the balance    
-def user_balance(balance):
-    print("Your balance is: ", balance)
 
 #function to show the transaction history
 def user_histroy():
@@ -100,15 +136,19 @@ root.geometry("800x600")
 
 shown_balance = Label(root, text=f"your balance is: ${balance}")
 shown_balance.grid(row=0, column=0, padx=5, pady=10,columnspan=4)
+#label used for messages
 label = Label(root, text=f"",fg="red")
 label.grid(row=1, column=0, padx=5, pady=10,columnspan=4,rowspan=2)
+
+#withdraw, deposit labels
+
 
 #buttons for user actions
 #deposit
 deposit_button = Button(root, text="Deposit", command=lambda:deposit())
 deposit_button.grid(row=4, column=0, padx=5, pady=10)
 #withdraw
-withdraw_button = Button(root, text="Withdraw", )
+withdraw_button = Button(root, text="Withdraw", command=lambda:withdraw())
 withdraw_button.grid(row=4, column=1, padx=5, pady=10)
 #transaction history
 history_button = Button(root, text="History", )
